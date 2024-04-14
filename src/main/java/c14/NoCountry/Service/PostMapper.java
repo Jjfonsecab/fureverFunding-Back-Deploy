2f2f7c064375910bcf7289fbb0124769858b39e0
Jjfonsecab <119600965+Jjfonsecab@users.noreply.github.com>
2024-04-14 17:54:35 -0500

    actualizando 2

--- src/main/java/c14/NoCountry/Service/PostMapper.java
+++ src/main/java/c14/NoCountry/Service/PostMapper.java
@@ -16,7 +16,7 @@ public class PostMapper {
                 .name(post.getName())
                 .data(post.getData())
                 .image(post.getImage())
-                .userId(post.getId())
+                .userId(post.getUser().getId())
                 .build();
     }
 
